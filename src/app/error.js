bc3288fedc805b0e8872290d2aa6f0743dbde4d0
natcurti <naticurti01@gmail.com>
2024-07-15 20:14:11 -0300

    Create error handling on initial page

--- src/app/error.js
+++ src/app/error.js
@@ -2,14 +2,28 @@
 
 import { useEffect } from "react";
 
+import Image from "next/image";
+import Link from "next/link";
+import { Heading } from "@/components/Heading";
+import style from "./error/error.module.css";
+import banner from "./error/500.png";
+import { ArrowBack } from "@/components/Icons/arrowBack";
+
 export default function Error({ error }) {
   useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
+    <div className={style.container}>
+      <Image src={banner} />
+      <Heading>Opa! Ocorreu um erro.</Heading>
+      <p className={style.text}>
+        Não conseguimos carregar a página, volte para seguir navegando.
+      </p>
+      <Link href="/" className={style.link}>
+        Voltar ao feed <ArrowBack color="#81FE88" />
+      </Link>
     </div>
   );
 }
